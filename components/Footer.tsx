fe3ab123c41d0cb066f8eff52372286898148ba9
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2023-12-16 23:10:08 +0530

    Update footer author name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,14 @@
 const Footer = () => {
   return (
     <div className="w-full text-xl text-center text-white p-3 h-auto">
-      Made with ❤️ by {" "}
+      Made by {" "}
       <a
         href="https://github.com/amitjimiwal"
         target="_blank"
         rel="noopener noreferrer"
         className="text-yellow-400 text-opacity-75"
       >
-        Amit Jimiwal
+        NotAmit
       </a>
     </div>
   );
